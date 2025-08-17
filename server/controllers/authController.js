de264280cd92fc053619a662c6a37a2688d2886c
ShayanBlaze <shayan.13.sharifi@gmail.com>
2025-08-17 11:52:47 +0330

    update: Increase token exp time

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -21,7 +21,7 @@ const registerUser = async (req, res) => {
     await user.save();
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
+      expiresIn: "7d",
     });
 
     res.status(StatusCodes.CREATED).json({
